f2c652847510fe851bcd25876bba612364773455
narmathakathirvel <113970699+narmathakathirvel@users.noreply.github.com>
2022-11-03 16:32:07 +0530

    Add files via upload

---
+++ Assignments/NARMATHA_K/Assignment-1/1st question/post.php
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: NARMATHA
+"""
+<?php 
+ 
+$name = $_POST['username']; 
+$phone = $_POST['phonenumber']; 
+ 
+echo "My name is $name <br /> and this is my mobile number {$phone}"; 
+ 
+?>
+
+
